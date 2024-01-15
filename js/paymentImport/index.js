9647023e40ec04f51ec4d2ab300e011e799ffba2
ivan soto <ivansotocaro@gmail.com>
2024-01-15 16:45:11 -0500

    feat: enviando archivo csv

--- js/paymentImport/index.js
+++ js/paymentImport/index.js
@@ -1,6 +1,11 @@
 let fileImportInput = document.querySelector("#fileImport");
 let earringsBtn = document.querySelector("#earringsBtn");
+let span = document.querySelector("#text-span");
 
+
+fileImportInput.addEventListener('change', function(){
+    span.textContent = fileImportInput.files[0].name;
+})
 
 let selectedFile;
 
@@ -9,39 +14,74 @@ let selectedFile;
   });
 
   earringsBtn.addEventListener("click", function () {
+    
+    let formData = new FormData();
+    formData.append("filecsv", fileImportInput.files[0]);
 
-    if (selectedFile) {
-      var fileReader = new FileReader();
-      fileReader.onload = function (event) {
-        var data = event.target.result;
+    $.ajax({
+      url: "http://127.0.0.1:8000/api/savepayment",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      statusCode: {
+        404: function () {
+          alert("La Ruta de la pagina no es la correcta");
+        },
+      },
+    })
+      .done(function (data) {
+        console.log("SUSSES  " + JSON.stringify(data));
+        // alert("¡Ingreso exitosamente!");
+        // if (data.status == "success") {
+        // }
+
+        // if (data.status == "error") {
+        //   alert("Error al guardar");
+        // }
+      })
+      .fail(function () {
+        alert("Algo salio mal");
+      });
+
+  })
+
+
+  // earringsBtn.addEventListener("click", function () {
+
+  //   if (selectedFile) {
+  //     var fileReader = new FileReader();
+  //     fileReader.onload = function (event) {
+  //       var data = event.target.result;
         
-        var workbook = XLSX.read(data, {
-          type: "binary",
-        });
+  //       var workbook = XLSX.read(data, {
+  //         type: "binary",
+  //         cellDates: true,
+  //       });
 
-        workbook.SheetNames.forEach((sheet) => {
+  //       workbook.SheetNames.forEach((sheet) => {
       
-          let rowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheet], {
-            header: 0,
-            range: 0,
-            defval: "",
-          });
+  //         let rowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheet], {
+  //           header: 0,
+  //           range: 0,
+  //           defval: "",
+  //         });
           
-          saveLocalStorage(rowObject);
-          // let jsonObject = JSON.stringify(rowObject);
-        });
-      };
-      fileReader.readAsBinaryString(selectedFile);
-      redirectPreview();
-    }
-  });
+  //         saveLocalStorage(rowObject);
+  //         // let jsonObject = JSON.stringify(rowObject);
+  //       });
+  //     };
+  //     fileReader.readAsBinaryString(selectedFile);
+  //     redirectPreview();
+  //   }
+  // });
 
 
 
-  function redirectPreview(){
-    window.location.href = 'preview.html';
-  }
+  // function redirectPreview(){
+  //   window.location.href = 'preview.html';
+  // }
 
-  function saveLocalStorage(data){
-    localStorage.setItem('preview', JSON.stringify(data) );
-  }
+  // function saveLocalStorage(data){
+  //   localStorage.setItem('preview', JSON.stringify(data) );
+  // }
--- js/preview/index.js
+++ js/preview/index.js
@@ -2,6 +2,39 @@ $(document).ready(function () {
   listaTablePreview();
 });
 
+let saveImportFile = document.querySelector("#saveImportFile"); 
+
+saveImportFile.addEventListener("click", function(){
+  let json = localStorage.getItem("preview");
+
+  $.ajax({
+    url: "http://127.0.0.1:8000/api/savepayment",
+    data: {
+      data: json,
+    },
+    type: "POST",
+    statusCode: {
+      404: function () {
+        alert.error("La Ruta de la pagina no es la correcta");
+      },
+    },
+  })
+    .done(function (data) {
+      console.log(data);
+      alert("¡Ingreso exitosamente!");
+      if (data.status == "success") {
+      }
+
+      if (data.status == "error") {
+        alert("Error al guardar");
+      }
+    })
+    .fail(function () {
+      alert("Algo salio mal");
+    });
+  
+})
+
 function listaTablePreview() {
   let payments = getDataLocalStorage();
     payments.forEach(function (payment) {
@@ -26,3 +59,15 @@ function getDataLocalStorage() {
 function deleteDataToLocalStorage(){
   localStorage.removeItem("preview");
 }
+
+
+// [
+//   {
+//     documento: 12345678,
+//     nombre: "admin",
+//     correo: "admin@gmail.com",
+//     monto: 50000,
+//     "fecha pago": 45300,
+//     "fecha limite": 45302,
+//   },
+// ];
